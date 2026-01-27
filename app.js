1c9d050563a91414a7bcc5613fa2fbeb4c6acc72
Pratush26 <pratushbarua26@gmail.com>
2026-01-27 23:22:42 +0600

    added folder structures

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+
+export const app = express()
+const allowedOrigins =
+  process.env.FRONTENDS?.split(",") || ["http://localhost:5173"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+import homeRoutes from "./routes/home.js";
+app.use("/", homeRoutes);
