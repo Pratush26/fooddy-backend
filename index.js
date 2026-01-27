91add452cc876a6f332ec201506e9390d401ed1b
Pratush26 <pratushbarua26@gmail.com>
2026-01-27 23:04:00 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`⚙️ Server is running at port : ${port}`))
